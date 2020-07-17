d3204b9d1c42f8d68576846dedaee4b452391edb
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-07-17 22:17:22 +0300

    did play and pause buttons and did some styles for video player

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.querySelector('#video');
+    const playButton = document.querySelector('#play-button');
+    const pauseButton = document.querySelector('#pause-button');
+
+    const play = () => {
+        video.play();
+    };
+
+    const pause = () => {
+        video.pause();
+    };
+
+    const addHandlers = () => {
+        playButton.addEventListener('click', play);
+        pauseButton.addEventListener('click', pause);
+
+    };
+
+    addHandlers();
+});
