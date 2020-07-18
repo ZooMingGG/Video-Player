afa170290dadaea037f1c06ecb8bbebf6fc7e781
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-07-18 11:23:37 +0300

    did cahnge volume function

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const video = document.querySelector('#video');
     const playButton = document.querySelector('#play-button');
     const pauseButton = document.querySelector('#pause-button');
+    const volumeBar = document.querySelector('#volume-bar');
+    const playProgressBar = document.querySelector('#play-progress-bar');
 
     const play = () => {
         video.play();
@@ -12,10 +14,15 @@ document.addEventListener('DOMContentLoaded', () => {
         video.pause();
     };
 
+    const changeVolume = (event) => {
+        let value = event.target.value;
+        video.volume = value / 100;
+    };
+
     const addHandlers = () => {
         playButton.addEventListener('click', play);
         pauseButton.addEventListener('click', pause);
-
+        volumeBar.addEventListener('input', changeVolume);
     };
 
     addHandlers();
