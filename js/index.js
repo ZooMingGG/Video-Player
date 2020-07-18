59d99ce1d8540bbbf0592209a65336e6bce30ee5
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-07-18 18:22:21 +0300

    did enter fullscreen function

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
+    const videoPlayer = document.querySelector('#video-player');
     const video = document.querySelector('#video');
     const playButton = document.querySelector('#play-button');
     const pauseButton = document.querySelector('#pause-button');
+    const fullscreenButton = document.querySelector('#fullscreen-button');
     const volumeBar = document.querySelector('#volume-bar');
     const playProgressBar = document.querySelector('#play-progress-bar');
 
@@ -26,12 +28,23 @@ document.addEventListener('DOMContentLoaded', () => {
         video.currentTime = video.duration / 100 * playProgressBar.value
     };
 
+    const enterFullscreen = () => {
+        if (videoPlayer.requestFullscreen) {
+            videoPlayer.requestFullscreen();
+        } else if (videoPlayer.mozRequestFullScreen) {
+            videoPlayer.mozRequestFullScreen();
+        } else if (videoPlayer.webkitRequestFullscreen) {
+            videoPlayer.webkitRequestFullscreen();
+        }
+    };
+
     const addHandlers = () => {
         playButton.addEventListener('click', play);
         pauseButton.addEventListener('click', pause);
         volumeBar.addEventListener('input', changeVolume);
         video.addEventListener('timeupdate', progressBarUpdate);
         playProgressBar.addEventListener('input', videoRewind);
+        fullscreenButton.addEventListener('click', enterFullscreen);
     };
 
     addHandlers();
